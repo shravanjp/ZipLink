1b158316c542a2f66cb51b2106e677aee081e0ef
ShravanJP <shravanjp8910@gmail.com>
2023-04-05 16:12:59 +0530

    refactor: add app to installed app and update database and template configuration

--- ZipLink/ZipLink/settings.py
+++ ZipLink/ZipLink/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,14 +31,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+# changes req
+
 INSTALLED_APPS = [
+    'urlshortener',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'urlshortner'
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'ZipLink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
